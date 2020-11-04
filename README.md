e9e2f8cac80ddd88bc7791d1de0934fa9750f491
Ben <limhanyangb@gmail.com>
2020-11-04 22:40:40 +0800

    fixed bugs

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # Soil sensor
-Everything is auto-configured from the SOC-Empty project from the BG22 Thunderboard Example Projects
+Everything is auto-configured from the SOC-Empty project from the BG22 Thunderboard Example Projects.
 
 # Components to add
 1. Simple Timer Service
-2. Simple Button -> btn0
-3. Simple LED -> led0
-4. USTIMER
-5. IADC
+2. Simple Button -> btn0 (PB01)
+3. Simple LED -> led0 (PB00)
+4. USART -> vcom (RX:PA06, TX:PA05)
+5. Log
+6. USTIMER
+7. IADC
+
+# Bluetooth GATT Configurator
+Add a new service with a Analog Characteristic
+
+1. New Service (Soil Sensor)
+   1. Analog (2 bytes, Notify, Read)
 
 # How to use
 Replace the app.c file with the app.c file provided in the repo.
+
+Compile the code and upload to a BG22 Thunderboard. You should see an output on a serial terminal of the ADC value, as well as the ADC counts.
--- app.c
+++ app.c
@@ -1,19 +1,3 @@
-/***************************************************************************/ /**
-                                                                               * @file
-                                                                               * @brief Core application logic.
-                                                                               *******************************************************************************
-                                                                               * # License
-                                                                               * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
-                                                                               *******************************************************************************
-                                                                               *
-                                                                               * The licensor of this software is Silicon Laboratories Inc. Your use of this
-                                                                               * software is governed by the terms of Silicon Labs Master Software License
-                                                                               * Agreement (MSLA) available at
-                                                                               * www.silabs.com/about-us/legal/master-software-license-agreement. This
-                                                                               * software is distributed to you in Source Code format and is governed by the
-                                                                               * sections of the MSLA applicable to Source Code.
-                                                                               *
-                                                                               ******************************************************************************/
 #include "app.h"
 
 #include "em_cmu.h"
@@ -24,8 +8,6 @@
 #include "sl_app_assert.h"
 #include "sl_app_log.h"
 #include "sl_bluetooth.h"
-#include "sl_i2cspm.h"
-#include "sl_i2cspm_instances.h"
 #include "sl_simple_button_instances.h"
 #include "sl_simple_led_instances.h"
 #include "sl_simple_timer.h"
